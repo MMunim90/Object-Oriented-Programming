4db2573854457f2ff6037493479efc715c60f009
MMunim <mmunim9.9.01@gmail.com>
2026-04-21 13:32:04 +0600

    polymorphism

--- 01_solution.py
+++ 01_solution.py
@@ -12,21 +12,32 @@ class Car:
     def full_name(self):
         return f"{self.__brand} {self.model}"
     
+    def fuel_type(self):
+        return "Petrol or Diesel"
+    
 class ElectricCar(Car):
     def __init__(self, brand, model, battery_size):
         super().__init__(brand, model)
         self.battery_size = battery_size
         
+    def fuel_type(self):
+        return "Electric Charge"
+
+        
 my_tesla = ElectricCar('Tesla', 'Model S', '85kWH')
 # print(my_tesla.model)
 # print(my_tesla.full_name())
 # print(my_tesla.__brand)
-print(my_tesla.get_brand())
+# print(my_tesla.get_brand())
     
-# my_car = Car("Toyota", "Corolla")
+my_car = Car("Toyota", "Corolla")
 # print(my_car.brand)
 # print(my_car.model)
 # print(my_car.full_name())
 
 # my_new_car = Car('Tata', 'Safari')
 # print(my_new_car.model)
+
+# polymorphism
+print(my_tesla.fuel_type())
+print(my_car.fuel_type())
